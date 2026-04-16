b8d260eea166e38809d06cc90ff9c926e65d79e4
carloshenriqueok <ifcarloshenrique@gmail.com>
2026-04-16 18:26:03 -0300

    FEAT: criação do componente do banner de delegação [REFS: #6]

---
+++ src/components/delegation/BannerDelegation.vue
@@ -0,0 +1,50 @@
+<script setup>
+// No momento esta estático, mas vai consumir a API de delegação no banco
+import { reactive } from 'vue';
+
+
+const delegation = reactive({
+    name: 'Colômbia',
+    officialName: 'República da Colômbia',
+    flag: '/colombia-flag.svg'
+});
+
+</script>
+
+<template>
+    <section class="banner">
+        <div class="delegation-name">
+            <div style="display: flex; gap: 1rem; align-items: center;">
+                <h2 class="name">{{ delegation.name }}</h2>
+                <img :src="delegation.flag" :alt="delegation.name">
+            </div>
+            <p class="official-name">{{ delegation.officialName }}</p>
+        </div>
+        <img src="/public/ods-shape.svg" alt="ods-shape" style="width: auto; height: 160px;">
+    </section>
+</template>
+
+<style scoped>
+.banner {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    border-radius: 10px;
+    background-color: #fff;
+}
+
+.delegation-name {
+
+    padding: 1em;
+
+    & .name {
+        font-weight: bold;
+    }
+
+    & .official-name {
+        color: #969696;
+        margin-top: 0.5rem;
+    }
+}
+
+</style>
--- src/views/DelagationView.vue
+++ src/views/DelagationView.vue
@@ -1,9 +1,13 @@
 <script setup>
 import AppHeaderMob from '@/components/layout/mobile/AppHeaderMob.vue';
+import BannerDelegation from '@/components/delegation/BannerDelegation.vue';
 </script>
 
 <template>
-<AppHeaderMob title="Delegação"/>
+    <AppHeaderMob title="Delegação"/>
+    <main>
+        <BannerDelegation />
+    </main>
 </template>
 
 <style scoped></style>
