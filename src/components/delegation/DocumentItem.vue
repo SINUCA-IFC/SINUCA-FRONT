36a64ffb8d688904fae168cfe54d6bc7c35b3a34
MCPavanello <pavanelloclaraa@gmail.com>
2026-08-19 15:44:56 -0300

    FEAT: html e css de DocumentsList e DocumentItem. #58

---
+++ src/components/delegation/DocumentItem.vue
@@ -0,0 +1,80 @@
+<script setup>
+function abrir () {
+    window.open("https://www.google.com", "_blank");
+}
+</script>
+<template>
+    <div class="geral" @click="abrir()">
+    <div class="separando">
+        <div class="span1">
+            <span class="mdi mdi-link-box-variant"></span>
+        </div>
+        <div class="textos">
+            <h5>Título sobre o link</h5>
+            <p>www.link.sinuca.com.br</p>
+        </div>
+    </div>
+        <div class="span2">
+            <span class="mdi mdi-open-in-new"></span>
+        </div>
+    </div>
+</template>
+<style scoped>
+.geral {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background-color: white;
+    border-radius: 0.5rem;
+    padding: 2rem 1.2rem;
+    cursor: pointer;
+    transition: 
+        transform 0.15s ease,
+        box-shadow 0.15s ease,
+        background-color 0.15s ease;
+
+
+    -webkit-tap-highlight-color: transparent;
+}
+
+.geral:hover {
+    background-color: #fafafa;
+    box-shadow: 0 4px 12px rgba(0, 0, 0, 0.08);
+    transform: translateY(-2px);
+}
+
+
+.geral:active {
+    transform: scale(0.98);
+    background-color: #f2f4f7;
+    box-shadow: none;
+}
+
+h5 {
+    font-size: 1.2rem;
+    font-weight: 600;
+    margin-bottom: 2vw;
+}
+
+p {
+    font-size: 0.9rem;
+}
+
+div.span1 span {
+    color: #01295F;
+    font-size: 2.5rem;
+    margin-right: 3vw;
+
+}
+
+div.separando {
+    display: flex;
+    align-items: center;
+}
+
+div.span2 span {
+    font-size: 1.5rem;
+    color: #969696;
+}
+
+</style>
--- src/components/delegation/DocumentsListComponent.vue
+++ src/components/delegation/DocumentsListComponent.vue
@@ -1,20 +1,55 @@
 <script setup>
 import { useLinkStore } from '@/stores/linkStore';
 import { onMounted } from 'vue';
+import DocumentItem from './DocumentItem.vue';
 
 const linkStore = useLinkStore();
 
-onMounted(() =>{
+onMounted(() => {
     linkStore.getLinks();
 });
 </script>
 
 <template>
-    <div class="header-section">
-        <h4 style="font-weight: bolder; font-size: 1.5rem; margin-bottom: 1.5rem;">Documentos</h4>
-        
+    <div class="header-section" >
+        <h4 style="font-weight: bolder; font-size: 1.5rem; margin-bottom: 1rem;">Documentos & links</h4>
     </div>
+    <h5 style="color: #01295F; font-size: 1.2rem; font-weight: 600; margin-bottom: 1vw;"> <span
+            class="mdi mdi-forum-outline" style="margin-right: 2vw;"></span>Para debate</h5>
+
+    <a href="https://pt.wikipedia.org/wiki/Blob_(ser_vivo)" style="text-decoration: underline;"><span
+            class="mdi mdi-help-circle-outline" style="margin-right: 2vw;"></span>Como funcionam os debates?</a>
+
+    <DocumentItem></DocumentItem>
+
+    <h5 style="color: #849324; font-size: 1.2rem; font-weight: 600; margin-bottom: 1vw;"> <span
+            class="mdi mdi-handshake-outline" style="margin-right: 2vw;"></span>Para mesas de cooperação</h5>
+
+    <a href="https://pt.wikipedia.org/wiki/Blob_(ser_vivo)" style="text-decoration: underline;"><span
+            class="mdi mdi-help-circle-outline" style="margin-right: 2vw;"></span>Como funcionam as mesas de
+        cooperação?</a>
+
+    <DocumentItem></DocumentItem>
+
+       <h5 style="color: #FFB30F; font-size: 1.2rem; font-weight: 600; margin-bottom: 1vw;"> <span
+            class="mdi mdi-handshake-outline" style="margin-right: 2vw;"></span>Para apresentações culturais</h5>
+
+    <a href="https://pt.wikipedia.org/wiki/Blob_(ser_vivo)" style="text-decoration: underline;"><span
+            class="mdi mdi-help-circle-outline" style="margin-right: 2vw;"></span>Como funcionam as apresentações culturais?</a>
+
+    <DocumentItem></DocumentItem>
+
+      <h5 style="color: #FD151B; font-size: 1.2rem; font-weight: 600; margin-bottom: 1vw;"> <span
+            class="mdi mdi-instagram" style="margin-right: 2vw;"></span>Para postagens</h5>
+
+    <a href="https://pt.wikipedia.org/wiki/Blob_(ser_vivo)" style="text-decoration: underline;"><span
+            class="mdi mdi-help-circle-outline" style="margin-right: 2vw;"></span>Como funcionam as postagens?</a>
+
+    <DocumentItem></DocumentItem>
 </template>
 
 <style scoped>
+a{
+        color: #437F97;
+}
 </style>
